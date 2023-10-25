6b3fa2b4e9025f960ee10dd7686ef93e7d890cd3
Beatriz Cristina Emerenciano <76975130+Beatriz-emerenciano@users.noreply.github.com>
2023-10-25 03:29:26 +0000

    funcao soma

--- main.c
+++ main.c
@@ -3,7 +3,17 @@
 #include "stdbool.h"
 
 int main() {
+int n,n2 ;
+
+int total;
+
     printf("Hello Word!");
     printf("testando");
+    printf("entre com um numero");
+    scanf("%d",&n);
+     printf("entre com um numero");
+    scanf("%d",&n2);
+
+    total = soma(n);
     
 }
