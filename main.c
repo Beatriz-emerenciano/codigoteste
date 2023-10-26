086cab60cefa69be5d2949b682c9c2265fa22ad8
Beatriz Cristina Emerenciano <76975130+Beatriz-emerenciano@users.noreply.github.com>
2023-10-26 19:24:44 +0000

    alteção no arquivo main

--- main.c
+++ main.c
@@ -3,19 +3,7 @@
 #include "stdbool.h"
 
 int main() {
-int n,n2 ;
 
-int total;
-
-    printf("Hello Word!");
-    printf("testando");
-    printf("entre com um numero");
-    scanf("%d",&n);
-     printf("entre com um numero");
-    scanf("%d",&n2);
-
-    total = soma(n);
-
-    printf("jovem programador");
+printf("usuario 1 fazendo alteração no código");
     
 }
